8859b9c4638dea292fe4bd30f3e4ddb45d9cc9ea
Samuel Wilder <pebazium@gmail.com>
2019-08-08 17:14:53 -0400

    Created sql queries for each of the python operations

--- main.py
+++ main.py
@@ -134,6 +134,35 @@ with split(word, str) as (
 		substr(str, instr(str, '|') + 1)
 	from split where str != ''
 ) select word from split where word != '' group by word;
+
+
+-- Profitibility by Genre:
+select 'Action', sum(gross - budget) as Profit from moviedata where instr(genres, 'Action') > 0;
+select 'Documentary', sum(gross - budget) as Profit from moviedata where instr(genres, 'Documentary') > 0;
+
+-- This may be it:
+select 'Action' as foo, * from moviedata where genres like foo;
+
+
+
+-- WORKS
+-- THIS IS IT!
+with all_genres (genre) as (
+	with split(word, str) as (
+		select
+			'', genres || '|'
+		from
+			moviedata
+		union all
+		select
+			substr(str, 0, instr(str, '|')),
+			substr(str, instr(str, '|') + 1)
+		from split where str != ''
+	) select word from split where word != '' group by word
+) select genre, sum(gross - budget) as Profit from all_genres join moviedata on instr(genres, genre) > 0
+group by genre
+order by Profit desc
+limit 10;
 '''
 # Top 10 Directors by profit
 
